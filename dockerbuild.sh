f7116305fea794bbbb5eb966f4532f8b78967f19
nick <nicola.asuni@miracl.com>
2016-05-26 15:36:05 +0100

    build this library inside a Docker container

---
+++ dockerbuild.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+#
+# dockerbuild.sh
+#
+# Build the software
+#
+# @author Nicola Asuni <nicola.asuni@miracl.com>
+# ------------------------------------------------------------------------------
+
+# NOTES:
+#
+# This script requires docker
+
+# EXAMPLE USAGE:
+# ./dockerbuild.sh
+
+# build the environment
+docker build -t miracl/cdev ./resources/DockerDev/
+
+# generate a docker file on the fly
+cat > Dockerfile <<- EOM
+FROM miracl/cdev
+MAINTAINER nicola.asuni@miracl.com
+RUN mkdir -p /root/.ssh
+RUN echo "Host github.com\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
+RUN mkdir -p /root/C/milagro-crypto-c
+ADD ./ /root/C/milagro-crypto-c
+RUN rm -rf /root/C/milagro-crypto-c/target/release
+WORKDIR /root/C/milagro-crypto-c
+RUN mkdir -p /root/C/milagro-crypto-c/target/release && \
+    cd target/release && \
+    cmake -D CMAKE_INSTALL_PREFIX=/opt/amcl -D USE_ANONYMOUS=on -D WORD_LENGTH=64 -D BUILD_WCC=on ../.. && \
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./ && \
+    make && \
+    make test && \
+    make doc && \
+    make package
+EOM
+
+# docker image name
+DOCKER_IMAGE_NAME="miracl/cdev"
+
+# build the docker container and build the project
+docker build --no-cache -t ${DOCKER_IMAGE_NAME} .
+
+# start a container using the newly created docker image
+CONTAINER_ID=$(docker run -d ${DOCKER_IMAGE_NAME})
+
+# copy the artifact back to the host
+docker cp ${CONTAINER_ID}:"/root/C/milagro-crypto-c/target" ./
+
+# remove the container and image
+docker rm -f ${CONTAINER_ID} || true
+docker rmi -f ${DOCKER_IMAGE_NAME} || true
