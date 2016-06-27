5c374dad1d918bc55a90153e485d6feaf1df5056
nick <nicola.asuni@miracl.com>
2016-06-27 21:56:32 +0100

    build windows version inside docker - tests are didable because binfmt limitation

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -15,7 +15,7 @@
 # ./dockerbuild.sh
 
 # build the environment
-docker build -t miracl/cdev ./resources/DockerDev/
+docker build --tag=miracl/cdev ./resources/DockerDev/
 
 # generate a docker file on the fly
 cat > Dockerfile <<- EOM
@@ -26,8 +26,9 @@ RUN echo "Host github.com\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 RUN mkdir -p /root/C/milagro-crypto-c
 ADD ./ /root/C/milagro-crypto-c
 WORKDIR /root/C/milagro-crypto-c
+
 RUN echo -e "\n\n*** BUILD FOR COVERAGE ***\n" && \
-rm -rf /root/C/milagro-crypto-c/target/build_test && \
+    rm -rf /root/C/milagro-crypto-c/target/build_test && \
     mkdir -p /root/C/milagro-crypto-c/target/build_test/coverage && \
     cd /root/C/milagro-crypto-c/target/build_test && \
     cmake -D CMAKE_BUILD_TYPE=Coverage -D CMAKE_INSTALL_PREFIX=/opt/amcl -D WORD_LENGTH=64 -D USE_ANONYMOUS=on -D BUILD_WCC=on ../.. && \
@@ -38,44 +39,46 @@ rm -rf /root/C/milagro-crypto-c/target/build_test && \
     make test && \
     lcov --no-checksum --directory . --capture --output-file coverage/amcl.info && \
     genhtml -o coverage -t "milagro-crypto-c Test Coverage" coverage/amcl.info && \
-    make doc && \
-echo -e "\n\n*** BUILD LINUX 64 ***\n\n" && \
+    make doc
+
+RUN echo -e "\n\n*** BUILD LINUX 64 ***\n\n" && \
     rm -rf /root/C/milagro-crypto-c/target/build_linux64 && \
     mkdir -p /root/C/milagro-crypto-c/target/build_linux64 && \
     cd /root/C/milagro-crypto-c/target/build_linux64 && \
     cmake -D CMAKE_INSTALL_PREFIX=/opt/amcl -D WORD_LENGTH=64 ../.. && \
     make && \
     make test && \
-    make package && \
-echo -e "\n\n*** BUILD LINUX 32 ***\n\n" && \
+    make package
+
+RUN echo -e "\n\n*** BUILD LINUX 32 ***\n\n" && \
     rm -rf /root/C/milagro-crypto-c/target/build_linux32 && \
     mkdir -p /root/C/milagro-crypto-c/target/build_linux32 && \
     cd /root/C/milagro-crypto-c/target/build_linux32 && \
     cmake -D CMAKE_INSTALL_PREFIX=/opt/amcl -D WORD_LENGTH=32 ../.. && \
     make && \
     make test && \
-    make package && \
-echo -e "\n\n*** BUILD WIN 64 ***\n\n" && \
+    make package
+
+RUN echo -e "\n\n*** BUILD WIN 64 ***\n\n" && \
     rm -rf /root/C/milagro-crypto-c/target/build_win64 && \
     mkdir -p /root/C/milagro-crypto-c/target/build_win64 && \
     cd /root/C/milagro-crypto-c/target/build_win64 && \
-    cmake -D CMAKE_TOOLCHAIN_FILE=../../resources/cmake/mingw64-cross.cmake -D CMAKE_INSTALL_PREFIX=/opt/amcl -D WORD_LENGTH=64 ../.. && \
-    make && \
-    make test && \
-echo -e "\n\n*** BUILD WIN 32 ***\n\n" && \
+    cmake -D CMAKE_TOOLCHAIN_FILE=../../resources/cmake/mingw64-cross.cmake -D WORD_LENGTH=64 ../.. && \
+    make
+
+RUN echo -e "\n\n*** BUILD WIN 32 ***\n\n" && \
     rm -rf /root/C/milagro-crypto-c/target/build_win32 && \
     mkdir -p /root/C/milagro-crypto-c/target/build_win32 && \
     cd /root/C/milagro-crypto-c/target/build_win32 && \
-    cmake -D CMAKE_TOOLCHAIN_FILE=../../resources/cmake/mingw32-cross.cmake -D CMAKE_INSTALL_PREFIX=/opt/amcl -D WORD_LENGTH=32 ../.. && \
-    make && \
-    make test
+    cmake -D CMAKE_TOOLCHAIN_FILE=../../resources/cmake/mingw32-cross.cmake -D WORD_LENGTH=32 ../.. && \
+    make
 EOM
 
 # docker image name
 DOCKER_IMAGE_NAME="local/build"
 
 # build the docker container and build the project
-docker build --no-cache -t ${DOCKER_IMAGE_NAME} .
+docker build --no-cache --tag=${DOCKER_IMAGE_NAME} .
 
 # start a container using the newly created docker image
 CONTAINER_ID=$(docker run -d ${DOCKER_IMAGE_NAME})
