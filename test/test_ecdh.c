efcb5b53cc9851ae7a4370f9ebe550df4ae928f3
Nicola Asuni <nicola.asuni@miracl.com>
2016-09-02 09:00:27 +0100

    format code

--- test/test_ecdh.c
+++ test/test_ecdh.c
@@ -36,241 +36,253 @@ typedef enum { false, true } bool;
 
 int main(int argc, char** argv)
 {
-  if (argc != 2) {
-    printf("usage: ./test_ecdh [path to test vector file]\n");
-    exit(EXIT_FAILURE);
-  }
-  int rc;
-  FILE * fp = NULL;
-  char line[LINE_LEN];
-  char * linePtr = NULL;
-  int l1=0;
-  int l2=0;
-
-  char QCAVSx[EGS];
-  const char* QCAVSxStr = "QCAVSx = ";
-  octet QCAVSxOct = {EGS,EGS,QCAVSx};
-
-#if CURVETYPE!=MONTGOMERY
-  char QCAVSy[EGS];
-  const char* QCAVSyStr = "QCAVSy = ";
-  octet QCAVSyOct = {EGS,EGS,QCAVSy};
-#endif
-
-  char * dIUT = NULL;
-  const char* dIUTStr = "dIUT = ";
-  octet dIUTOct;
-
-  char QIUTx[EGS];
-  const char* QIUTxStr = "QIUTx = ";
-  octet QIUTxOct = {EGS,EGS,QIUTx};
-
-#if CURVETYPE!=MONTGOMERY
-  char QIUTy[EGS];
-  const char* QIUTyStr = "QIUTy = ";
-  octet QIUTyOct = {EGS,EGS,QIUTy};
-#endif
-
-  char * ZIUT = NULL;
-  const char* ZIUTStr = "ZIUT = ";
-  octet ZIUTOct;
-
-  char q[2*EFS+1];
-  octet QOct={0,sizeof(q),q};
-  char z[EFS];
-  octet ZOct={0,sizeof(z),z};
-
-  fp = fopen(argv[1], "r");
-  if (fp == NULL) {
-      printf("ERROR opening test vector file\n");
-      exit(EXIT_FAILURE);
-  }
-
-  bool readLine = false;
-  int i=0; 
-  while (fgets(line, LINE_LEN, fp) != NULL)  {
-    i++;
-    readLine = true;
-    if (!strncmp(line, QCAVSxStr, strlen(QCAVSxStr))) {
-#ifdef DEBUG
-      printf("line %d %s\n", i,line);	
-#endif
-      // Find hex value in string
-      linePtr = line + strlen(QCAVSxStr);
-
-      // Allocate data
-      l1 = strlen(linePtr)-1;
-
-      // QCAVSx binary value
-      amcl_hex2bin(linePtr, QCAVSx, l1);
-    }
-
-#if CURVETYPE!=MONTGOMERY
-    if (!strncmp(line, QCAVSyStr, strlen(QCAVSyStr))) {
-#ifdef DEBUG
-      printf("line %d %s\n", i,line);	
-#endif
-      // Find hex value in string
-      linePtr = line + strlen(QCAVSyStr);
-
-      // Allocate data
-      l1 = strlen(linePtr)-1;
-
-      // QCAVSy binary value
-      amcl_hex2bin(linePtr, QCAVSy, l1);
-    }
-#endif
-
-    if (!strncmp(line, dIUTStr, strlen(dIUTStr))) {
-#ifdef DEBUG
-      printf("line %d %s\n", i,line);	
-#endif
-      // Find hex value in string
-      linePtr = line + strlen(dIUTStr);
-
-      // Allocate memory
-      l1 = strlen(linePtr)-1;
-      l2 = l1/2;
-      dIUT = (char*) malloc (l2);
-      if (dIUT==NULL) 
+    if (argc != 2)
+    {
+        printf("usage: ./test_ecdh [path to test vector file]\n");
         exit(EXIT_FAILURE);
-
-      // dIUT binary value
-      amcl_hex2bin(linePtr, dIUT, l1);
-
-      dIUTOct.len=l2;
-      dIUTOct.max=l2;
-      dIUTOct.val=dIUT;
     }
+    int rc;
+    FILE * fp = NULL;
+    char line[LINE_LEN];
+    char * linePtr = NULL;
+    int l1=0;
+    int l2=0;
 
-    if (!strncmp(line, QIUTxStr, strlen(QIUTxStr))) {
-#ifdef DEBUG
-      printf("line %d %s\n", i,line);	
-#endif
-      // Find hex value in string
-      linePtr = line + strlen(QIUTxStr);
-
-      // Allocate data
-      l1 = strlen(linePtr)-1;
-
-      // QIUTx binary value
-      amcl_hex2bin(linePtr, QIUTx, l1);
-    }
+    char QCAVSx[EGS];
+    const char* QCAVSxStr = "QCAVSx = ";
+    octet QCAVSxOct = {EGS,EGS,QCAVSx};
 
 #if CURVETYPE!=MONTGOMERY
-    if (!strncmp(line, QIUTyStr, strlen(QIUTyStr))) {
-#ifdef DEBUG
-      printf("line %d %s\n", i,line);	
-#endif
-      // Find hex value in string
-      linePtr = line + strlen(QIUTyStr);
-
-      // Allocate data
-      l1 = strlen(linePtr)-1;
-
-      // QIUTy binary value
-      amcl_hex2bin(linePtr, QIUTy, l1);
-    }
+    char QCAVSy[EGS];
+    const char* QCAVSyStr = "QCAVSy = ";
+    octet QCAVSyOct = {EGS,EGS,QCAVSy};
 #endif
 
-    if (!strncmp(line, ZIUTStr, strlen(ZIUTStr))) {
-#ifdef DEBUG
-      printf("line %d %s\n", i,line);	
-#endif
-      // Find hex value in string
-      linePtr = line + strlen(ZIUTStr);
+    char * dIUT = NULL;
+    const char* dIUTStr = "dIUT = ";
+    octet dIUTOct;
 
-      // Allocate memory
-      l1 = strlen(linePtr)-1;
-      l2 = l1/2;
-      ZIUT = (char*) malloc (l2);
-      if (ZIUT==NULL) 
+    char QIUTx[EGS];
+    const char* QIUTxStr = "QIUTx = ";
+    octet QIUTxOct = {EGS,EGS,QIUTx};
+
+#if CURVETYPE!=MONTGOMERY
+    char QIUTy[EGS];
+    const char* QIUTyStr = "QIUTy = ";
+    octet QIUTyOct = {EGS,EGS,QIUTy};
+#endif
+
+    char * ZIUT = NULL;
+    const char* ZIUTStr = "ZIUT = ";
+    octet ZIUTOct;
+
+    char q[2*EFS+1];
+    octet QOct= {0,sizeof(q),q};
+    char z[EFS];
+    octet ZOct= {0,sizeof(z),z};
+
+    fp = fopen(argv[1], "r");
+    if (fp == NULL)
+    {
+        printf("ERROR opening test vector file\n");
         exit(EXIT_FAILURE);
+    }
 
-      // ZIUT binary value
-      amcl_hex2bin(linePtr, ZIUT, l1);
+    bool readLine = false;
+    int i=0;
+    while (fgets(line, LINE_LEN, fp) != NULL)
+    {
+        i++;
+        readLine = true;
+        if (!strncmp(line, QCAVSxStr, strlen(QCAVSxStr)))
+        {
+#ifdef DEBUG
+            printf("line %d %s\n", i,line);
+#endif
+            // Find hex value in string
+            linePtr = line + strlen(QCAVSxStr);
 
-      ZIUTOct.len=l2;
-      ZIUTOct.max=l2;
-      ZIUTOct.val=ZIUT;
-  
-      // Assign QIUT
-      char q1[2*EFS+1];
-      octet QIUTOct={0,sizeof(q1),q1};
+            // Allocate data
+            l1 = strlen(linePtr)-1;
+
+            // QCAVSx binary value
+            amcl_hex2bin(linePtr, QCAVSx, l1);
+        }
+
 #if CURVETYPE!=MONTGOMERY
-      QIUTOct.val[0]=4;
-      QIUTOct.len=1;
-      OCT_joctet(&QIUTOct,&QIUTxOct);
-      OCT_joctet(&QIUTOct,&QIUTyOct);
+        if (!strncmp(line, QCAVSyStr, strlen(QCAVSyStr)))
+        {
+#ifdef DEBUG
+            printf("line %d %s\n", i,line);
+#endif
+            // Find hex value in string
+            linePtr = line + strlen(QCAVSyStr);
+
+            // Allocate data
+            l1 = strlen(linePtr)-1;
+
+            // QCAVSy binary value
+            amcl_hex2bin(linePtr, QCAVSy, l1);
+        }
+#endif
+
+        if (!strncmp(line, dIUTStr, strlen(dIUTStr)))
+        {
+#ifdef DEBUG
+            printf("line %d %s\n", i,line);
+#endif
+            // Find hex value in string
+            linePtr = line + strlen(dIUTStr);
+
+            // Allocate memory
+            l1 = strlen(linePtr)-1;
+            l2 = l1/2;
+            dIUT = (char*) malloc (l2);
+            if (dIUT==NULL)
+                exit(EXIT_FAILURE);
+
+            // dIUT binary value
+            amcl_hex2bin(linePtr, dIUT, l1);
+
+            dIUTOct.len=l2;
+            dIUTOct.max=l2;
+            dIUTOct.val=dIUT;
+        }
+
+        if (!strncmp(line, QIUTxStr, strlen(QIUTxStr)))
+        {
+#ifdef DEBUG
+            printf("line %d %s\n", i,line);
+#endif
+            // Find hex value in string
+            linePtr = line + strlen(QIUTxStr);
+
+            // Allocate data
+            l1 = strlen(linePtr)-1;
+
+            // QIUTx binary value
+            amcl_hex2bin(linePtr, QIUTx, l1);
+        }
+
+#if CURVETYPE!=MONTGOMERY
+        if (!strncmp(line, QIUTyStr, strlen(QIUTyStr)))
+        {
+#ifdef DEBUG
+            printf("line %d %s\n", i,line);
+#endif
+            // Find hex value in string
+            linePtr = line + strlen(QIUTyStr);
+
+            // Allocate data
+            l1 = strlen(linePtr)-1;
+
+            // QIUTy binary value
+            amcl_hex2bin(linePtr, QIUTy, l1);
+        }
+#endif
+
+        if (!strncmp(line, ZIUTStr, strlen(ZIUTStr)))
+        {
+#ifdef DEBUG
+            printf("line %d %s\n", i,line);
+#endif
+            // Find hex value in string
+            linePtr = line + strlen(ZIUTStr);
+
+            // Allocate memory
+            l1 = strlen(linePtr)-1;
+            l2 = l1/2;
+            ZIUT = (char*) malloc (l2);
+            if (ZIUT==NULL)
+                exit(EXIT_FAILURE);
+
+            // ZIUT binary value
+            amcl_hex2bin(linePtr, ZIUT, l1);
+
+            ZIUTOct.len=l2;
+            ZIUTOct.max=l2;
+            ZIUTOct.val=ZIUT;
+
+            // Assign QIUT
+            char q1[2*EFS+1];
+            octet QIUTOct= {0,sizeof(q1),q1};
+#if CURVETYPE!=MONTGOMERY
+            QIUTOct.val[0]=4;
+            QIUTOct.len=1;
+            OCT_joctet(&QIUTOct,&QIUTxOct);
+            OCT_joctet(&QIUTOct,&QIUTyOct);
 #else
-      QIUTOct.val[0]=2;
-      QIUTOct.len=1;
-      OCT_joctet(&QIUTOct,&QIUTxOct);
-#endif 
+            QIUTOct.val[0]=2;
+            QIUTOct.len=1;
+            OCT_joctet(&QIUTOct,&QIUTxOct);
+#endif
 
-      // Assign QCAVS
-      char q2[2*EFS+1];
-      octet QCAVSOct={0,sizeof(q2),q2};
+            // Assign QCAVS
+            char q2[2*EFS+1];
+            octet QCAVSOct= {0,sizeof(q2),q2};
 #if CURVETYPE!=MONTGOMERY
-      QCAVSOct.val[0]=4;
-      QCAVSOct.len=1;
-      OCT_joctet(&QCAVSOct,&QCAVSxOct);
-      OCT_joctet(&QCAVSOct,&QCAVSyOct);
+            QCAVSOct.val[0]=4;
+            QCAVSOct.len=1;
+            OCT_joctet(&QCAVSOct,&QCAVSxOct);
+            OCT_joctet(&QCAVSOct,&QCAVSyOct);
 #else
-      QCAVSOct.val[0]=2;
-      QCAVSOct.len=1;
-      OCT_joctet(&QCAVSOct,&QCAVSxOct);
+            QCAVSOct.val[0]=2;
+            QCAVSOct.len=1;
+            OCT_joctet(&QCAVSOct,&QCAVSxOct);
 #endif
 
 #ifdef DEBUG
-      printf("dIUTOct: ");
-      OCT_output(&dIUTOct);
-      printf("\n");
+            printf("dIUTOct: ");
+            OCT_output(&dIUTOct);
+            printf("\n");
 #endif
 
-      // Check correct public key generated
-      ECP_KEY_PAIR_GENERATE(NULL,&dIUTOct,&QOct);
-      rc = OCT_comp(&QOct,&QIUTOct);
-      if (!rc) {
-        printf("ERROR: TEST ECDH KEYPAIR FAILED LINE %d\n",i);
-#ifdef DEBUG    
-      printf("QOct: ");
-      OCT_output(&QOct);
-      printf("QIUTOct: ");
-      OCT_output(&QIUTOct);
-      printf("\n");
+            // Check correct public key generated
+            ECP_KEY_PAIR_GENERATE(NULL,&dIUTOct,&QOct);
+            rc = OCT_comp(&QOct,&QIUTOct);
+            if (!rc)
+            {
+                printf("ERROR: TEST ECDH KEYPAIR FAILED LINE %d\n",i);
+#ifdef DEBUG
+                printf("QOct: ");
+                OCT_output(&QOct);
+                printf("QIUTOct: ");
+                OCT_output(&QIUTOct);
+                printf("\n");
 #endif
-        exit(EXIT_FAILURE);
-      }
+                exit(EXIT_FAILURE);
+            }
 
-      // Check correct shared value generated
-      ECPSVDP_DH(&dIUTOct,&QCAVSOct,&ZOct);
-      rc = OCT_comp(&ZOct,&ZIUTOct);
-      if (!rc) {
-        printf("TEST ECDH Z FAILED LINE %d\n",i);
-        exit(EXIT_FAILURE);
-      }
+            // Check correct shared value generated
+            ECPSVDP_DH(&dIUTOct,&QCAVSOct,&ZOct);
+            rc = OCT_comp(&ZOct,&ZIUTOct);
+            if (!rc)
+            {
+                printf("TEST ECDH Z FAILED LINE %d\n",i);
+                exit(EXIT_FAILURE);
+            }
 
 #ifdef DEBUG
-      printf("ZOct: ");
-      OCT_output(&ZOct);
-      printf("\n");
-      printf("ZIUTOct: ");
-      OCT_output(&ZIUTOct);
-      printf("\n");
+            printf("ZOct: ");
+            OCT_output(&ZOct);
+            printf("\n");
+            printf("ZIUTOct: ");
+            OCT_output(&ZIUTOct);
+            printf("\n");
 #endif
 
-      free(dIUT);
-      dIUT = NULL;
-      free(ZIUT);
-      ZIUT = NULL;
+            free(dIUT);
+            dIUT = NULL;
+            free(ZIUT);
+            ZIUT = NULL;
+        }
     }
-  }
-  fclose(fp);
-  if (!readLine) {
-    printf("ERROR Empty test vector file\n");
-    exit(EXIT_FAILURE);
-  }
-  printf("SUCCESS TEST ECDH KEYPAIR PASSED\n");
-  exit(EXIT_SUCCESS);
+    fclose(fp);
+    if (!readLine)
+    {
+        printf("ERROR Empty test vector file\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("SUCCESS TEST ECDH KEYPAIR PASSED\n");
+    exit(EXIT_SUCCESS);
 }
--- test/test_ecdsa_verify.c
+++ test/test_ecdsa_verify.c
@@ -36,202 +36,224 @@ typedef enum { false, true } bool;
 
 int main(int argc, char** argv)
 {
-  if (argc != 3) {
-    printf("usage: ./test_ecdsa_sign [path to test vector file] [hash type: sha256||sha512] \n");
-    exit(EXIT_FAILURE);
-  }
-  int rc;
-  bool pass;
-  FILE * fp = NULL;
-  char line[LINE_LEN];
-  char * linePtr = NULL;
-  int l1=0;
-  int l2=0;
-  char * Msg = NULL;
-  const char* MsgStr = "Msg = ";
-  octet MsgOct;
-  char Qx[EGS];
-  const char* QxStr = "Qx = ";
-  octet QxOct = {EGS,EGS,Qx};
-  char Qy[EGS];
-  const char* QyStr = "Qy = ";
-  octet QyOct = {EGS,EGS,Qy};
-  char * R = NULL;
-  const char* RStr = "R = ";
-  octet ROct;
-  char * S = NULL;
-  const char* SStr = "S = ";
-  octet SOct;
-  const char* ResultStr = "Result = ";
-
-  // Assign hash type
-  int hash_type;
-  if (!strcmp(argv[2], "sha256")) {
-    hash_type = 32;
-  } else if (!strcmp(argv[2], "sha384")) { 
-    hash_type = 48;
-  } else if (!strcmp(argv[2], "sha512")) { 
-    hash_type = 64;
-  } else { 
-    hash_type = 32;
-  }
-
-  fp = fopen(argv[1], "r");
-  if (fp == NULL) {
-      printf("ERROR opening test vector file\n");
-      exit(EXIT_FAILURE);
-  }
-
-  bool readLine = false;
-
-  int i=0; 
-  while (fgets(line, LINE_LEN, fp) != NULL)  {
-    i++;
-    readLine = true;
-    if (!strncmp(line, MsgStr, strlen(MsgStr))) {
-#ifdef DEBUG
-      printf("line %d %s\n", i,line);	
-#endif
-      // Find hex value in string
-      linePtr = line + strlen(MsgStr);
-
-      // Allocate memory
-      l1 = strlen(linePtr)-1;
-      l2 = l1/2;
-      Msg = (char*) malloc (l2);
-      if (Msg==NULL) 
+    if (argc != 3)
+    {
+        printf("usage: ./test_ecdsa_sign [path to test vector file] [hash type: sha256||sha512] \n");
         exit(EXIT_FAILURE);
+    }
+    int rc;
+    bool pass;
+    FILE * fp = NULL;
+    char line[LINE_LEN];
+    char * linePtr = NULL;
+    int l1=0;
+    int l2=0;
+    char * Msg = NULL;
+    const char* MsgStr = "Msg = ";
+    octet MsgOct;
+    char Qx[EGS];
+    const char* QxStr = "Qx = ";
+    octet QxOct = {EGS,EGS,Qx};
+    char Qy[EGS];
+    const char* QyStr = "Qy = ";
+    octet QyOct = {EGS,EGS,Qy};
+    char * R = NULL;
+    const char* RStr = "R = ";
+    octet ROct;
+    char * S = NULL;
+    const char* SStr = "S = ";
+    octet SOct;
+    const char* ResultStr = "Result = ";
 
-      // Msg binary value
-      amcl_hex2bin(linePtr, Msg, l1);
-
-      MsgOct.len=l2;
-      MsgOct.max=l2;
-      MsgOct.val=Msg;
+    // Assign hash type
+    int hash_type;
+    if (!strcmp(argv[2], "sha256"))
+    {
+        hash_type = 32;
+    }
+    else if (!strcmp(argv[2], "sha384"))
+    {
+        hash_type = 48;
+    }
+    else if (!strcmp(argv[2], "sha512"))
+    {
+        hash_type = 64;
+    }
+    else
+    {
+        hash_type = 32;
     }
 
-    if (!strncmp(line, QxStr, strlen(QxStr))) {
-#ifdef DEBUG
-      printf("line %d %s\n", i,line);	
-#endif
-      // Find hex value in string
-      linePtr = line + strlen(QxStr);
-
-      // Allocate data
-      l1 = strlen(linePtr)-1;
-
-      // Qx binary value
-      amcl_hex2bin(linePtr, Qx, l1);
-    }
-
-    if (!strncmp(line, QyStr, strlen(QyStr))) {
-#ifdef DEBUG
-      printf("line %d %s\n", i,line);	
-#endif
-      // Find hex value in string
-      linePtr = line + strlen(QyStr);
-
-      // Allocate data
-      l1 = strlen(linePtr)-1;
-
-      // Qy binary value
-      amcl_hex2bin(linePtr, Qy, l1);
-    }
-
-    if (!strncmp(line, RStr, strlen(RStr))) {
-#ifdef DEBUG
-      printf("line %d %s\n", i,line);	
-#endif
-      // Find hex value in string
-      linePtr = line + strlen(RStr);
-
-      // Allocate memory
-      l1 = strlen(linePtr)-1;
-      l2 = l1/2;
-      R = (char*) malloc (l2);
-      if (R==NULL) 
+    fp = fopen(argv[1], "r");
+    if (fp == NULL)
+    {
+        printf("ERROR opening test vector file\n");
         exit(EXIT_FAILURE);
-
-      // R binary value
-      amcl_hex2bin(linePtr, R, l1);
-
-      ROct.len=l2;
-      ROct.max=l2;
-      ROct.val=R;
     }
 
-    if (!strncmp(line, SStr, strlen(SStr))) {
+    bool readLine = false;
+
+    int i=0;
+    while (fgets(line, LINE_LEN, fp) != NULL)
+    {
+        i++;
+        readLine = true;
+        if (!strncmp(line, MsgStr, strlen(MsgStr)))
+        {
 #ifdef DEBUG
-      printf("line %d %s\n", i,line);	
+            printf("line %d %s\n", i,line);
 #endif
-      // Find hex value in string
-      linePtr = line + strlen(SStr);
+            // Find hex value in string
+            linePtr = line + strlen(MsgStr);
 
-      // Allocate memory
-      l1 = strlen(linePtr)-1;
-      l2 = l1/2;
-      S = (char*) malloc (l2);
-      if (S==NULL) 
-        exit(EXIT_FAILURE);
+            // Allocate memory
+            l1 = strlen(linePtr)-1;
+            l2 = l1/2;
+            Msg = (char*) malloc (l2);
+            if (Msg==NULL)
+                exit(EXIT_FAILURE);
 
-      // S binary value
-      amcl_hex2bin(linePtr, S, l1);
+            // Msg binary value
+            amcl_hex2bin(linePtr, Msg, l1);
 
-      SOct.len=l2;
-      SOct.max=l2;
-      SOct.val=S;
-    }
+            MsgOct.len=l2;
+            MsgOct.max=l2;
+            MsgOct.val=Msg;
+        }
 
-    if (!strncmp(line, ResultStr, strlen(ResultStr))) {
+        if (!strncmp(line, QxStr, strlen(QxStr)))
+        {
 #ifdef DEBUG
-      printf("line %d %s\n", i,line);	
+            printf("line %d %s\n", i,line);
 #endif
-      linePtr = line + strlen(ResultStr);
-      char r1[1];
-      char r2[1] = {"P"};
-      strncpy(r1,linePtr,1);
-      if (r1[0] == r2[0]) {
-        pass = true;
-      } else {
-        pass = false;
-      }
+            // Find hex value in string
+            linePtr = line + strlen(QxStr);
 
-      // Assign Public Key to EC
-      BIG qx, qy;
-      char q[2*EFS+1];
-      BIG_fromBytes(qx,QxOct.val);
-      BIG_fromBytes(qy,QyOct.val);
-      octet QOct={sizeof(q),sizeof(q),q};
-      QOct.val[0]=4;
-      BIG_toBytes(&(QOct.val[1]),qx);
-      BIG_toBytes(&(QOct.val[EFS+1]),qy);
-     
-      rc = ECPVP_DSA(hash_type,&QOct,&MsgOct,&ROct,&SOct);
-      // Test expected to pass. rc is true for fail
-      if ( pass && rc ) {
-        printf("TEST ECDSA VERIFY FAILED LINE %d pass %d rc %d\n",i,pass,rc);
-        exit(EXIT_FAILURE);
-      }
+            // Allocate data
+            l1 = strlen(linePtr)-1;
 
-      // Test expected to fail
-      if ( !pass && !rc ) {
-        printf("TEST ECDSA VERIFY FAILED LINE %d pass %d rc %d\n",i,pass,rc);
-        exit(EXIT_FAILURE);
-      }
+            // Qx binary value
+            amcl_hex2bin(linePtr, Qx, l1);
+        }
 
-      free(Msg);
-      Msg = NULL;
-      free(R);
-      R = NULL;
-      free(S);
-      S = NULL;
+        if (!strncmp(line, QyStr, strlen(QyStr)))
+        {
+#ifdef DEBUG
+            printf("line %d %s\n", i,line);
+#endif
+            // Find hex value in string
+            linePtr = line + strlen(QyStr);
+
+            // Allocate data
+            l1 = strlen(linePtr)-1;
+
+            // Qy binary value
+            amcl_hex2bin(linePtr, Qy, l1);
+        }
+
+        if (!strncmp(line, RStr, strlen(RStr)))
+        {
+#ifdef DEBUG
+            printf("line %d %s\n", i,line);
+#endif
+            // Find hex value in string
+            linePtr = line + strlen(RStr);
+
+            // Allocate memory
+            l1 = strlen(linePtr)-1;
+            l2 = l1/2;
+            R = (char*) malloc (l2);
+            if (R==NULL)
+                exit(EXIT_FAILURE);
+
+            // R binary value
+            amcl_hex2bin(linePtr, R, l1);
+
+            ROct.len=l2;
+            ROct.max=l2;
+            ROct.val=R;
+        }
+
+        if (!strncmp(line, SStr, strlen(SStr)))
+        {
+#ifdef DEBUG
+            printf("line %d %s\n", i,line);
+#endif
+            // Find hex value in string
+            linePtr = line + strlen(SStr);
+
+            // Allocate memory
+            l1 = strlen(linePtr)-1;
+            l2 = l1/2;
+            S = (char*) malloc (l2);
+            if (S==NULL)
+                exit(EXIT_FAILURE);
+
+            // S binary value
+            amcl_hex2bin(linePtr, S, l1);
+
+            SOct.len=l2;
+            SOct.max=l2;
+            SOct.val=S;
+        }
+
+        if (!strncmp(line, ResultStr, strlen(ResultStr)))
+        {
+#ifdef DEBUG
+            printf("line %d %s\n", i,line);
+#endif
+            linePtr = line + strlen(ResultStr);
+            char r1[1];
+            char r2[1] = {"P"};
+            strncpy(r1,linePtr,1);
+            if (r1[0] == r2[0])
+            {
+                pass = true;
+            }
+            else
+            {
+                pass = false;
+            }
+
+            // Assign Public Key to EC
+            BIG qx, qy;
+            char q[2*EFS+1];
+            BIG_fromBytes(qx,QxOct.val);
+            BIG_fromBytes(qy,QyOct.val);
+            octet QOct= {sizeof(q),sizeof(q),q};
+            QOct.val[0]=4;
+            BIG_toBytes(&(QOct.val[1]),qx);
+            BIG_toBytes(&(QOct.val[EFS+1]),qy);
+
+            rc = ECPVP_DSA(hash_type,&QOct,&MsgOct,&ROct,&SOct);
+            // Test expected to pass. rc is true for fail
+            if ( pass && rc )
+            {
+                printf("TEST ECDSA VERIFY FAILED LINE %d pass %d rc %d\n",i,pass,rc);
+                exit(EXIT_FAILURE);
+            }
+
+            // Test expected to fail
+            if ( !pass && !rc )
+            {
+                printf("TEST ECDSA VERIFY FAILED LINE %d pass %d rc %d\n",i,pass,rc);
+                exit(EXIT_FAILURE);
+            }
+
+            free(Msg);
+            Msg = NULL;
+            free(R);
+            R = NULL;
+            free(S);
+            S = NULL;
+        }
     }
-  }
-  fclose(fp);
-  if (!readLine) {
-    printf("ERROR Empty test vector file\n");
-    exit(EXIT_FAILURE);
-  }
-  printf("SUCCESS TEST ECDSA %s VERIFY PASSED\n", argv[2]);
-  exit(EXIT_SUCCESS);
+    fclose(fp);
+    if (!readLine)
+    {
+        printf("ERROR Empty test vector file\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("SUCCESS TEST ECDSA %s VERIFY PASSED\n", argv[2]);
+    exit(EXIT_SUCCESS);
 }
