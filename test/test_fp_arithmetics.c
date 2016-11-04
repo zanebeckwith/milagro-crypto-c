f16e29784795faf348e7746a1ece85c2286c186b
Alessandro Budroni <alessandro.budroni@miracl.com>
2016-11-04 16:59:25 +0000

    Add FP tests for C25519, C41417, GOLDILOCKS, MS255, MS256, NIST521.[MCC-5]

--- test/test_fp_arithmetics.c
+++ test/test_fp_arithmetics.c
@@ -60,7 +60,6 @@ void read_DBIG(DBIG A, char* string)
 
 int main(int argc, char** argv)
 {
-
     if (argc != 2)
     {
         printf("usage: ./test_FP_arithmetics [path to test vector file]\n");
