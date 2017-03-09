1e0e511d5f1ce8e49a52cc2bdde55d9718e45d21
Alessandro Budroni <alessandro.budroni@miracl.com>
2017-03-09 15:07:18 +0000

    Add first RSA test in golang.

--- wrappers/go/rsa_test.go
+++ wrappers/go/rsa_test.go
@@ -17,16 +17,49 @@ specific language governing permissions and limitations
 under the License.
 */
 
-/*package amcl
+package amcl
 
 import (
-	"crypto/rand"
 	"encoding/hex"
 	"fmt"
-	mathrand "math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-*/
+func TestRSA(t *testing.T) {
+
+	// Seed value for Random Number Generator (RNG)
+	seedHex := "9e8b4178790cd57a5761c4a6f164ba72"
+	seed, err := hex.DecodeString(seedHex)
+	if err != nil {
+		fmt.Println("Error decoding seed value")
+		return
+	}
+
+	rng := RSACreateCSPRNG(seed)
+
+	// Message to encrypt
+	MESSAGEstr := "Hello World\n"
+	MESSAGE := []byte(MESSAGEstr)
+
+	// Generating public/private key pair
+    RSA_PrivKey, RSA_PubKey := RSA_KeyPair(&rng,65537,nil,nil);
+
+    // OAEP encode MESSAGE to e
+	_, F := OAEP_ENCODE(HASH_TYPE_RSA, MESSAGE, &rng, nil)
+
+	// encrypt encoded MESSAGE
+	G := RSA_ENCRYPT(&RSA_PubKey, F[:])
+
+	// decrypt encrypted MESSAGE
+    ML := RSA_DECRYPT(&RSA_PrivKey,G[:])
+
+    // OAEP decode MESSAGE
+    _, Fgot := OAEP_DECODE(HASH_TYPE_RSA, nil, ML[:])
+
+    assert.Equal(t, F, Fgot, "Should be equal")
+}
+
+
+
