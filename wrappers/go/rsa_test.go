dc295f178d813d0c4fe02fbc867d8b95ef21ab2a
Alessandro Budroni <alessandro.budroni@miracl.com>
2017-03-10 12:23:28 +0000

    Update version and format code.

--- wrappers/go/rsa_test.go
+++ wrappers/go/rsa_test.go
@@ -23,8 +23,6 @@
  * under the License.
  */
 
-
-
 package amcl
 
 import (
@@ -52,24 +50,24 @@ func TestRSA(t *testing.T) {
 	MESSAGE := []byte(MESSAGEstr)
 
 	// Generating public/private key pair
-    RSA_PrivKey, RSA_PubKey := RSA_KeyPair(&rng,65537,nil,nil);
+	RSA_PrivKey, RSA_PubKey := RSA_KeyPair(&rng, 65537, nil, nil)
 
-    // OAEP encode MESSAGE to e
+	// OAEP encode MESSAGE to e
 	_, F := OAEP_ENCODE(HASH_TYPE_RSA, MESSAGE, &rng, nil)
 
 	// encrypt encoded MESSAGE
 	G := RSA_ENCRYPT(&RSA_PubKey, F[:])
 
 	// decrypt encrypted MESSAGE
-    ML := RSA_DECRYPT(&RSA_PrivKey, G[:])
+	ML := RSA_DECRYPT(&RSA_PrivKey, G[:])
 
-    // OAEP decode MESSAGE
-    _, Fgot := OAEP_DECODE(HASH_TYPE_RSA, nil, ML[:])
+	// OAEP decode MESSAGE
+	_, Fgot := OAEP_DECODE(HASH_TYPE_RSA, nil, ML[:])
 
-    // destroy private key
-    RSA_PRIVATE_KEY_KILL(&RSA_PrivKey)
+	// destroy private key
+	RSA_PRIVATE_KEY_KILL(&RSA_PrivKey)
 
-    assert.Equal(t, Fgot, MESSAGE, "Should be equal")
+	assert.Equal(t, Fgot, MESSAGE, "Should be equal")
 }
 
 func TestRsaSign(t *testing.T) {
@@ -85,23 +83,22 @@ func TestRsaSign(t *testing.T) {
 	rng := CreateCSPRNG(seed)
 
 	// Generating public/private key pair
-    RSA_PrivKey, RSA_PubKey := RSA_KeyPair(&rng,65537,nil,nil);
+	RSA_PrivKey, RSA_PubKey := RSA_KeyPair(&rng, 65537, nil, nil)
 
 	// Message to encrypt
 	MESSAGEstr := "Hello World\n"
 	MESSAGE := []byte(MESSAGEstr)
 
-    // Signing message
-    _, C := PKCS15(HASH_TYPE_RSA,MESSAGE);
+	// Signing message
+	_, C := PKCS15(HASH_TYPE_RSA, MESSAGE)
 
-    // create signature in S
-    S := RSA_DECRYPT(&RSA_PrivKey,C[:])
+	// create signature in S
+	S := RSA_DECRYPT(&RSA_PrivKey, C[:])
 
-    Cgot := RSA_ENCRYPT(&RSA_PubKey,S[:]);
+	Cgot := RSA_ENCRYPT(&RSA_PubKey, S[:])
 
-    // destroy private key
-    RSA_PRIVATE_KEY_KILL(&RSA_PrivKey)
+	// destroy private key
+	RSA_PRIVATE_KEY_KILL(&RSA_PrivKey)
 
-    assert.Equal(t, C, Cgot, "Should be equal")
+	assert.Equal(t, C, Cgot, "Should be equal")
 }
-
