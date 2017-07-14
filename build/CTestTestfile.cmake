194b0313921c4b829b1f512780f368e0456d10d3
Alessandro <alessandro.budroni@miracl.com>
2017-07-14 17:18:46 +0100

    Add src and a few tests.

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alessandro/Dev/milagro-crypto-c3
-# Build directory: /home/alessandro/Dev/milagro-crypto-c3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("include")
--- build/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alessandro/Dev/milagro-crypto-c3/include
-# Build directory: /home/alessandro/Dev/milagro-crypto-c3/build/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
